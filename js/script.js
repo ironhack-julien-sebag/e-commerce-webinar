960f9190cf234a162ef7d9972e6c1a421cbbfda2
Julien Sebag <julien.sebag@me.com>
2021-09-24 15:51:49 +0200

    Finished webinar

--- js/script.js
+++ js/script.js
@@ -23,3 +23,63 @@ let items = [
         image: "https://d0bb7f9bf11b5ad1a6b2-6175f06f5e3f64e15abbf67415a276ec.ssl.cf1.rackcdn.com/product-images/designlab/11-oz-traditional-ceramic-coffee-mugs-7102-white1582888132.jpg",
     },
 ]
+
+let cart = []
+
+// Variables
+let listContainer = document.querySelector("#items")
+
+items.forEach((item, i) => {
+    listContainer.innerHTML += `<li>
+        <img src="${item.image}" alt="${item.name}" />
+        <div class="content">
+            <p class="title">${item.name}</p>
+            <p class="price">$${item.price}</p>
+
+            <span class="input-container">
+                <label>Quantity</label>
+                <input type="number" min="0" placeholder="0" onchange="inputChange(${i}, '${item.name}', '${item.price}', '${item.image}')" />
+            </span>
+            
+            <button>Buy</button>
+        </div>
+    </li>`
+})
+
+function showCart() {
+    let cartContainer = document.querySelector("#cart")
+    cartContainer.innerHTML = ""
+
+    let total = 0
+
+    cart.forEach(item => {
+        total += item.price * item.quantity
+        cartContainer.innerHTML += `<li>
+        <img src="${item.image}" alt="${item.name}" />
+        <div class="content">
+            <p class="title">${item.name}</p>
+            <p class="price">$${item.price}</p>
+            <p>Quantity: ${item.quantity}</p>
+        </div>
+    </li>`
+    })
+
+    document.querySelector("#grandTotal").innerHTML = total
+}
+
+function inputChange(i, name, price, image) {
+    let listItem = document.querySelectorAll("li")[i]
+    let input = listItem.querySelector("input")
+    let button = listItem.querySelector("button")
+
+    button.onclick = function () {
+        cart.push({
+            quantity: input.value,
+            name: name,
+            price: price,
+            image: image,
+        })
+        showCart()
+    }
+}
+
